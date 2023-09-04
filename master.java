c9d90c230295b3c1dfed28b3c08337d02893851c
island248 <ilynd1125@gmail.com>
2023-09-04 12:30:27 -0400

    Created the help function

--- master.java
+++ master.java
@@ -3,6 +3,48 @@
 
 public class master{
    public static void main(String args[]){
-
+      help();
 } 
+public static void help()
+{
+
+   String [] commandLines = {
+      "New Puzzle",
+      "Base Puzzle",
+      "Show Puzzle",
+      "Found Words",
+      "Guess",
+      "Shuffle",
+      "Save Puzzle",
+      "Save Current",
+      "Load Puzzle",
+      "Show Status",
+      "Exit"
+   };
+   String [] explanations = {
+      "Generates a new puzzle with 7 unique letters and a required letter",
+      "Generates a new puzzle with a word of the player's choice with 7 unique letters and a required letter",
+      "Shows the current puzzle the player is working on",
+      "Generates a list of words that the player has found ",
+      "Allows the player to guess their words",
+      "Allows the player to shuffle around the words",
+      "Lets the player save a blank puzzle",
+      "Lets the players save a puzzle that may have been partially played",
+      "The player can load a saved game",
+      "The player can see their rank and progress on a current puzzle",
+      "Leave the application"
+   };
+   System.out.println("The Spelling Bee game allows players to create words using 7 unique letters with a required letter. ");
+   System.out.println("- Words must contain at least 4 letters");
+   System.out.println("- Words must include the required letter");
+   System.out.println("- Letters can be used more than once");
+   System.out.println("");
+   System.out.println("Command Line   |   Explanation");
+
+        System.out.println("---------------------------------------");
+        for (int i = 0; i < commandLines.length; i++) {
+         System.out.printf("%-15s |   %s%n", commandLines[i], explanations[i]);
+        }
 }
+}
+
