5a4259c28bfdeac5ce16b126bdde8ef060611b82
logan82902 <107006937+logan82902@users.noreply.github.com>
2023-09-04 16:43:09 -0400

    resolving conflicts

--- master.java
+++ master.java
@@ -10,7 +10,7 @@ import java.util.*;
 /**********************************************************/
 /**********************************************************/
 
-public class master{
+public class master extends helpers{
 
 public static void main(String args[]) throws FileNotFoundException{ 
     String baseWord = getBaseWord(dictionaryFile());
@@ -57,90 +57,81 @@ public static char getReqLetter(String baseWord){
 /**********************************************************/
 /**********************************************************/
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/*
+ * pointsPWord
+ * param: String userGuess
+ * returns: int 
+ * This function recieves a String as a parameter,
+ * the length of the string is then retrived. After 
+ * the amount of pointes is determined be the length
+ * it will return that point value.
+ */
+
+private static int pointsPWord(String userGuess){
+    int length = userGuess.length();
+
+    int points = 0;
+
+    switch(length){
+        case 4:
+        points = 2;
+        break;
+        case 5:
+        points = 5;
+        break;
+        case 6:
+        points = 7;
+        break;
+        case 7:
+        points = 9;
+        break;
+        default:
+        points = 10;
+        break;
+    }
+    
+    return points;
+}
+
+/*********************************************************/
+/*********************************************************/
+
+/*
+ * playerRank
+ * param: int playerPoints
+ * returns: String
+ * This function receives an int as a parameter, the int value
+ * is used to determine the rank of the player. 
+ */
+
+private static String playerRank(int playerPoints){
+
+    String playerRank = "";
+
+    if (isBetween(playerPoints, 0, 99)){
+        playerRank += "Novice";
+    }
+    if (isBetween(playerPoints, 100, 199)){
+        playerRank += "Amateur";
+    }
+    if (isBetween(playerPoints, 200, 299)){
+        playerRank += "Expert";
+    }
+    if(isBetween(playerPoints, 300, 399)){
+        playerRank += "Master";
+    }
+    if(isBetween(playerPoints, 400, 499)){
+        playerRank += "Legend";
+    }
+    if(playerPoints >= 500){
+        playerRank += "Immortal";
+    }
+
+    return playerRank;
+}
+
+/*********************************************************/
+/*********************************************************/
 
 
 
@@ -184,9 +175,3 @@ public static char getReqLetter(String baseWord){
 
 
 }
-
-
-
-
-
-
